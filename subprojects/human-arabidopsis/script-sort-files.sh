0f6d3f627c355ad11d17597030988beb3bcfdc46
fa2k <pmb@fa2k.net>
2020-05-16 23:20:07 +0200

    new analysis to look at correlations in size dopsis/human

---
+++ subprojects/human-arabidopsis/script-sort-files.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+SAMPLE=NEB-100ng-1
+
+singularity run -B ../../20_piccard/$SAMPLE.bam:/input.bam \
+                -B $PWD/data/:/data \
+                ../../container-images/picard-2.20.4.simg SortSam \
+                I=/input.bam \
+                O=/data/$SAMPLE-human-qsort.bam \
+                SO=queryname \
+                CREATE_INDEX=true
+
+
+singularity run -B ../../arabidopsis/20_piccard/$SAMPLE.bam:/input.bam \
+                -B $PWD/data/:/data \
+                ../../container-images/picard-2.20.4.simg SortSam \
+                I=/input.bam \
+                O=/data/$SAMPLE-arabidopsis-qsort.bam \
+                SO=queryname \
+                CREATE_INDEX=true
